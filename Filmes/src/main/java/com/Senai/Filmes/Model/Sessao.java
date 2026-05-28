f31c0717b7a4061bafc82ee5c10b31dfac76d7e4
guibenat <guibenattedev@gmail.com>
2026-05-28 19:28:23 -0300

    Feat: create class sessao

---
+++ Filmes/src/main/java/com/Senai/Filmes/Model/Sessao.java
@@ -0,0 +1,14 @@
+package com.Senai.Filmes.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "sessoes")
+public class Sessao {
+
+}
