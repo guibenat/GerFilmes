ba8f57282f94ca14b26c08644c517ad461d4a3b0
guibenat <guibenattedev@gmail.com>
2026-05-28 20:19:35 -0300

    Feat: class Usuario completed

--- Filmes/src/main/java/com/Senai/Filmes/Model/Filme.java
+++ Filmes/src/main/java/com/Senai/Filmes/Model/Filme.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @Entity
 @Data
-@Table(name = "cFilme")
+@Table(name = "filmes")
 
 public class Filme {
 
--- Filmes/src/main/java/com/Senai/Filmes/Model/Sessao.java
+++ Filmes/src/main/java/com/Senai/Filmes/Model/Sessao.java
@@ -1,14 +1,40 @@
 package com.Senai.Filmes.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @Entity
 @Table(name = "sessoes")
 public class Sessao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
+    @ManyToOne
+    @JoinColumn(name = "filme_id")
+    @NotNull
+    private Filme filme;
+
+    @ManyToOne
+    @JoinColumn(name = "sala_id")
+    @NotNull
+    private Sala sala;
+
+    @NotNull(message = "O horario do inicio e obtigatorio")
+    private LocalDateTime inicio;
+
+    @NotNull(message = "O horario do fim é obrigatorio")
+    private LocalDateTime fim;
+
+    @NotNull(message = "O preço é obrigatório")
+    private BigDecimal preco;
 }
