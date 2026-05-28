ec7bd33baafa37512e7c81bfefa6d83f838a9ab0
guibenat <guibenattedev@gmail.com>
2026-05-27 21:38:46 -0300

    Feat: finished class assento

--- Filmes/src/main/java/com/Senai/Filmes/Model/Assentos.java
+++ Filmes/src/main/java/com/Senai/Filmes/Model/Assentos.java
@@ -1,6 +1,8 @@
 package com.Senai.Filmes.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +17,12 @@ public class Assentos {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @NotBlank
+    private String fileira;
+
+    @NotNull
+    private int numero;
 
     @ManyToOne
     @JoinColumn(name = "sala_id")
