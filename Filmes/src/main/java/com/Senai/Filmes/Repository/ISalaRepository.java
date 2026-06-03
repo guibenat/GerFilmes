541c606c5b26a2356ff6ca8871b3fe70b4a1bd17
guibenat <guibenattedev@gmail.com>
2026-06-02 21:33:10 -0300

    Feat: finished class SalaService

--- Filmes/src/main/java/com/Senai/Filmes/Repository/ISalaRepository.java
+++ Filmes/src/main/java/com/Senai/Filmes/Repository/ISalaRepository.java
@@ -2,11 +2,12 @@ package com.Senai.Filmes.Repository;
 
 import com.Senai.Filmes.Model.Assentos;
 import com.Senai.Filmes.Model.Filme;
+import com.Senai.Filmes.Model.Sala;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ISalaRepository extends JpaRepository <Assentos, UUID>{
+public interface ISalaRepository extends JpaRepository <Sala, UUID>{
 }
--- Filmes/src/main/java/com/Senai/Filmes/Service/SalaService.java
+++ Filmes/src/main/java/com/Senai/Filmes/Service/SalaService.java
@@ -1,8 +1,53 @@
 package com.Senai.Filmes.Service;
 
+import com.Senai.Filmes.DTO.Request.SalaRequest;
+import com.Senai.Filmes.DTO.Response.SalaResponse;
+import com.Senai.Filmes.Model.Assentos;
+import com.Senai.Filmes.Model.Sala;
+import com.Senai.Filmes.Repository.ISalaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SalaService {
 
+    @Autowired
+    private ISalaRepository salaRepository;
+
+    public SalaResponse cadastrarSala(SalaRequest request) {
+        Sala sala = new Sala();
+        sala.setNome(request.nome());
+        sala.setTotalAssentos(request.fileiras() * request.assentosPorFileira());
+
+        List<Assentos> assentos = gerarAssentos(sala, request.fileiras(), request.assentosPorFileira());
+        sala.setAssentos(assentos);
+
+        return toResponse(salaRepository.save(sala));
+    }
+
+    private List<Assentos> gerarAssentos(Sala sala, int fileiras, int assentosPorFileira) {
+        List<Assentos> assentos = new ArrayList<>();
+        for (int f = 0; f < fileiras; f++) {
+            String fileira = String.valueOf((char) ('A' + f));
+            for (int n = 1; n <= assentosPorFileira; n++) {
+                Assentos assento = new Assentos();
+                assento.setSala(sala);
+                assento.setFileira(fileira);
+                assento.setNumero(n);
+                assentos.add(assento);
+            }
+        }
+        return assentos;
+    }
+
+    private SalaResponse toResponse(Sala sala) {
+        return new SalaResponse(
+                sala.getId(),
+                sala.getNome(),
+                sala.getTotalAssentos()
+        );
+    }
 }
