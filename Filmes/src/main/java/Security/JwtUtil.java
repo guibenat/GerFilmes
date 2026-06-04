7afacca8240b2301a1a107763fa48efc7cd4a0eb
guibenat <guibenattedev@gmail.com>
2026-06-03 21:47:04 -0300

    Feat: import dependencys if spring security

--- Filmes/src/main/java/Security/JwtUtil.java
+++ Filmes/src/main/java/Security/JwtUtil.java
@@ -1,4 +1,18 @@
 package Security;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+
 public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getChave() {
+        return Keys.hmacShaKeyFor()
+    }
 }
