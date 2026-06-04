ac26c38205e06e7455143bb07e9cf78bf4e8ca22
guibenat <guibenattedev@gmail.com>
2026-06-03 21:39:08 -0300

    Feat: create security part

--- Filmes/src/main/java/com/Senai/Filmes/Service/AutenticacaoService.java
+++ Filmes/src/main/java/com/Senai/Filmes/Service/AutenticacaoService.java
@@ -2,6 +2,8 @@ package com.Senai.Filmes.Service;
 
 import com.Senai.Filmes.Repository.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,4 +14,15 @@ public class AutenticacaoService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+
 }
