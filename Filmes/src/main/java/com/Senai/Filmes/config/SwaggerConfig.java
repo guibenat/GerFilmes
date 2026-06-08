56ad43c73f482d4f67ad9b1ec6c4c8817b615aa4
guibenat <guibenattedev@gmail.com>
2026-06-07 23:12:30 -0300

    Feat: class SwaggerConfig completed

--- Filmes/src/main/java/com/Senai/Filmes/config/SwaggerConfig.java
+++ Filmes/src/main/java/com/Senai/Filmes/config/SwaggerConfig.java
@@ -1,5 +1,24 @@
 package com.Senai.Filmes.config;
 
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Cinema Api")
+                .description("Api para gerenciamento de cinema")
+                .version("1.0.0")).addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer").bearerFormat("JWT")));
+
+    }
 }
