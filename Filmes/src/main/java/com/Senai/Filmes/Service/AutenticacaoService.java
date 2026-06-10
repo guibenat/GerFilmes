6b48c4256ea69138921e825d4ad84e7277db0c1e
guibenat <guibenattedev@gmail.com>
2026-06-09 21:50:02 -0300

    Feat: updates

--- Filmes/src/main/java/com/Senai/Filmes/Service/AutenticacaoService.java
+++ Filmes/src/main/java/com/Senai/Filmes/Service/AutenticacaoService.java
@@ -1,10 +1,16 @@
 package com.Senai.Filmes.Service;
 
+import com.Senai.Filmes.DTO.Request.LoginRequest;
+import com.Senai.Filmes.DTO.Response.AuthResponse;
+import com.Senai.Filmes.Model.Usuario;
 import com.Senai.Filmes.Security.JwtUtil;
 import com.Senai.Filmes.Repository.IUsuarioRepository;
 import com.Senai.Filmes.Security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +32,12 @@ public class AutenticacaoService {
     @Autowired
     private UserDetailsServiceImpl userDetailsServiceImpl;
 
-
-
+    public AuthResponse login(LoginRequest loginRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.senha())
+        );
+        Usuario usuario = usuarioRepository.findByEmail(loginRequest.email())
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado!"));
+        UserDetails userDetails =
+    }
 }
