cbb3ad9f4f16f9105ee810bc5d03fbac9c57a116
guibenat <guibenattedev@gmail.com>
2026-06-10 21:15:31 -0300

    Fix: collum expered in Sessao

--- Filmes/src/main/java/com/Senai/Filmes/Model/Sessao.java
+++ Filmes/src/main/java/com/Senai/Filmes/Model/Sessao.java
@@ -30,12 +30,13 @@ public class Sessao {
     private Sala sala;
 
     @NotNull(message = "O horario do inicio e obtigatorio")
-    private LocalDateTime inicioDia;
+    private LocalDateTime inicio;
 
     @NotNull(message = "O horario do fim é obrigatorio")
     private LocalDateTime fim;
 
     @NotNull(message = "O inicio da sessãoprecisa ser informado.")
+    private LocalDateTime inicioSessao;
 
     @NotNull(message = "O preço é obrigatório")
     private BigDecimal preco;
